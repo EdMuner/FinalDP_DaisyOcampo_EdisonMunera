5a3d5203bf9254d15d20dd65e702f6d74005928c
EdisonMunera <edisonmunera72@gmail.com>
2019-11-26 18:40:56 -0500

    Implementation proyect TipCalculator

---
+++ TipCalculator.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using TipCalculator.Core.ViewModels;
+
+namespace Factorial.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<TipViewModel>();
+        }
+    }
+}
---
+++ TipCalculator.Core/Services/CalculationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipCalculator.Core.Services
+{
+    namespace TipCalculator.Core.Services
+    {
+        public class CalculationService : ICalculationService
+        {
+            public decimal TipAmount(decimal subTotal, double generosity)
+            {
+                return subTotal * (decimal)(generosity / 100);
+            }
+        }
+    }
+
+}
